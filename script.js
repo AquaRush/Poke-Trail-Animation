0772cc40267dc92784b6b5fb2ccaaba454557470
AquaRush <george.mihaylov96@gmail.com>
2024-04-24 18:33:27 +0300

    Update script.js
    Added more Pokémon images.

--- script.js
+++ script.js
@@ -10,6 +10,13 @@ let pokemonImages = [
     'url(https://cdn0.iconfinder.com/data/icons/pokemon-go-vol-1/135/_jigglypuff-512.png)',
     'url(https://cdn0.iconfinder.com/data/icons/pokemon-go-vol-2/135/_weedle-256.png)',
     'url(https://cdn0.iconfinder.com/data/icons/pokemon-go-vol-2/135/_snorlax-512.png)',
+    'url(https://cdn0.iconfinder.com/data/icons/pokemon-go-vol-2/135/_venonat-512.png)',
+    'url(https://cdn0.iconfinder.com/data/icons/pokemon-go-vol-2/135/_pidgey-512.png)',
+    'url(https://cdn0.iconfinder.com/data/icons/pokemon-go-vol-2/135/_squirtle-512.png)',
+    'url(https://cdn3.iconfinder.com/data/icons/pokemon-go-3/512/pokemon_go_play_game_charcter_2-256.png)',
+    'url(https://cdn0.iconfinder.com/data/icons/pokemon-go-vol-1/135/_dratini-256.png)',
+    'url(https://cdn0.iconfinder.com/data/icons/pokemon-go-vol-1/135/_bulbasaur-512.png)',
+    'url(https://cdn0.iconfinder.com/data/icons/pokemon-go-vol-1/135/_abra-256.png)',
 ]
 
 body.addEventListener("mousemove", (mouse) => {
