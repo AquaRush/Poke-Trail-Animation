df3d5fa36ff5d01f2115abc1232940f86ebbbbb9
AquaRush <george.mihaylov96@gmail.com>
2024-04-24 15:09:40 +0300

    Initial commit.

---
+++ script.js
@@ -0,0 +1,44 @@
+const body = document.querySelector("body");
+
+let pokemonImages = [
+    'url(https://cdn0.iconfinder.com/data/icons/pokemon-go-vol-1/135/_charmander-256.png)',
+    'url(https://cdn0.iconfinder.com/data/icons/pokemon-go-vol-2/135/_psyduck-512.png)',
+    'url(https://cdn0.iconfinder.com/data/icons/pokemon-go-vol-1/135/_mankey-512.png)',
+    'url(https://cdn0.iconfinder.com/data/icons/pokemon-go-vol-1/135/_eevee-512.png)',
+    'url(https://cdn0.iconfinder.com/data/icons/pokemon-go-vol-1/135/_caterpie-512.png)',
+    'url(https://cdn0.iconfinder.com/data/icons/pokemon-go-vol-2/135/_pikachu-512.png)',
+    'url(https://cdn0.iconfinder.com/data/icons/pokemon-go-vol-1/135/_jigglypuff-512.png)',
+    'url(https://cdn0.iconfinder.com/data/icons/pokemon-go-vol-2/135/_weedle-256.png)',
+    'url(https://cdn0.iconfinder.com/data/icons/pokemon-go-vol-2/135/_snorlax-512.png)',
+]
+
+body.addEventListener("mousemove", (mouse) => {
+
+    // Get X & Y coordinates of the mouse
+    let xMousePos = mouse.offsetX;
+    let yMousePos = mouse.offsetY;
+    let span = document.createElement("span");
+    body.appendChild(span);
+
+    span.style.left = xMousePos + "px";
+    span.style.top = yMousePos + "px";
+
+    // Randomise the size of the icon
+    let randomSize = Math.random() * 80;
+    span.style.width = randomSize + "px";
+    span.style.height = randomSize + "px";
+
+    // Randomise the image
+    span.style.backgroundImage = pokemonImages[randomPokeImage()];
+
+    // Remove the element for performance
+    setTimeout(() => {
+        span.remove();
+    }, 14000); // 14 seconds since CSS animation takes 15s, anything after that will be removed to save memory.
+});
+
+function randomPokeImage() {
+
+    // Get random image index
+    return Math.floor(Math.random() * pokemonImages.length);
+}
